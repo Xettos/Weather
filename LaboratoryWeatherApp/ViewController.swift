5f1932d48638565909d3562a600e0c4010679645
Ivan <xettss@gmail.com>
2020-09-22 13:05:19 +0300

    Initial Commit

---
+++ LaboratoryWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaboratoryWeatherApp
+//
+//  Created by Иван Гришечко on 22.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
